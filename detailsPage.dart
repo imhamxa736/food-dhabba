37550af74d24f7e545c4b3f32b2f2589dae8b2fb
imhamxa736 <67777795+imhamxa736@users.noreply.github.com>
2020-09-22 12:41:15 +0500

    Add files via upload

---
+++ detailsPage.dart
@@ -0,0 +1,297 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class DetailsPage extends StatefulWidget {
+  String heroTag;
+  String foodName;
+  String foodPrice;
+
+  DetailsPage ({this.heroTag,this.foodName,this.foodPrice});
+  @override
+  _DetailsPageState createState() => _DetailsPageState();
+}
+
+class _DetailsPageState extends State<DetailsPage> {
+  var selectedCard = 'WEIGHT';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: (){ Navigator.of(context).pop(); },
+
+          icon: Icon(Icons.arrow_back_ios),
+          color: Colors.white,
+          iconSize: 20.0,
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        title: Text('Details',style: TextStyle(fontSize: 20.0,color: Colors.white),),
+        centerTitle: true,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.more_horiz),
+            onPressed: (){},
+            color: Colors.white,
+            iconSize: 20.0,
+          )
+        ],
+      ),
+      body: ListView(
+        children: <Widget>[
+
+          Stack(
+            children: <Widget>[
+              Container(
+                height: MediaQuery.of(context).size.height-80.0,
+                width: MediaQuery.of(context).size.width,
+                color: Colors.transparent,
+              ),
+              Positioned(
+                top: 75.0,
+                child: Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(80.0),
+                        topLeft: Radius.circular(80.0),
+                      ),
+                      color: Colors.white
+                  ),
+                  height: MediaQuery.of(context).size.height-100.0,
+                  width: MediaQuery.of(context).size.width,
+                ),
+              ),
+
+
+              // FOr the IMAGE APPERAING__________
+              Positioned(
+                top: 30.0,
+                left: (MediaQuery.of(context).size.width/2)-100.0,
+                child: Hero(
+                  tag: widget.heroTag,
+                  child: Container(
+                    decoration: BoxDecoration(
+
+                        borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(80.0),
+                          topLeft: Radius.circular(80.0),
+                          bottomLeft: Radius.circular(80.0),
+                          bottomRight:Radius.circular(80.0)
+                        ),
+                      
+                      image: DecorationImage(
+                        image: AssetImage(widget.heroTag),
+                        fit: BoxFit.cover
+                      )
+                    ),
+                    height: 150.0,
+                    width: 150.0,
+                  ),
+                ),
+              ),
+
+
+              // FOR THE BUTTONS ETC ON THE SECOND SCREEN-------
+              Positioned(
+                top: 230.0,
+                left: 30.0,
+                right: 30.0,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget> [
+                    Text(widget.foodName,style: TextStyle(
+                      color: Colors.blue,
+                      fontSize: 24.0
+                    ),),
+                    SizedBox(height: 10.0,),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text(widget.foodPrice,style: TextStyle(
+                          color: Colors.black54,
+                          fontSize: 18.0
+                        ),),
+                        Container(
+                          color: Colors.blue,
+                          height: 18.0,
+                          width: 1.0,
+                        ),
+                        Container(
+                          width: 120.0,
+                          height: 30.0,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(17.0),
+                            color: Colors.blue
+                          ),
+                        child: Row
+                          (mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: <Widget>[
+                            InkWell(
+                              onTap: (){},
+                              child: Container(
+                                height: 25.0,
+                                width: 25.0,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(7.0),
+                                  color: Colors.blueAccent
+                                ),
+                                child: Center(
+                                  child: Icon(
+                                    Icons.remove,
+                                    color: Colors.white,
+                                    size: 20.0,
+                                  ),
+                                ),
+                              ),
+                            ),
+
+                            Text('2',style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16.0
+                            ),
+                            ),
+
+                            InkWell(
+                              onTap: (){},
+                              child: Container(
+                                height: 25.0,width: 25.0,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(7.0),
+                                  color: Colors.blueAccent
+                                ),
+                                child: Center(
+                                  child: Icon(
+                                    Icons.add_circle,
+                                    color: Colors.white,
+                                    size: 20.0,
+                                  ),
+                                ),
+
+                              ),
+                            )
+                          ],
+
+                        ),
+                        ),
+
+                      ],
+                    ),
+
+                    SizedBox(height: 16.0),
+
+                    Container(
+                      height: 150.0,
+                      child: ListView(
+                        scrollDirection: Axis.horizontal,
+                        children: <Widget>[
+_buildInfoCard('WEIGHT', '300', 'G'),
+                          SizedBox(width: 10.0),
+                          _buildInfoCard('Calories', '267', 'CAL'),
+                          SizedBox(width: 10.0,),
+                          _buildInfoCard("VITAMINS", 'A,D2', 'VIT'),
+                          SizedBox(width: 10.0,),
+                          _buildInfoCard('AVAIL', 'NO','AV')
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 20.0,),
+                    Padding(
+                      padding: EdgeInsets.only(bottom: 5.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.only(topLeft: Radius.circular(10.0), topRight: Radius.circular(10.0), bottomLeft: Radius.circular(25.0), bottomRight: Radius.circular(25.0)),
+                          color: Colors.black
+                        ),
+                        height: 50.0,
+                        child: Center(
+                          child: Text(
+                            '\$52.00',style: TextStyle(
+                            color: Colors.white
+                          )
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                )
+              )
+            ]
+          )
+
+        ]
+      )
+    );
+  }
+
+
+Widget _buildInfoCard (String cardTitle, String info, String unit){
+  return InkWell(
+    onTap: (){
+      selectCard (cardTitle);
+    },
+    child: AnimatedContainer(
+      duration: Duration(milliseconds:650 ),
+      curve: Curves.bounceInOut,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        color: cardTitle== selectedCard ? Colors.blueGrey: Colors.white,
+        border: Border.all(
+          color: cardTitle==selectedCard?
+              Colors.transparent:
+              Colors.blueGrey.withOpacity(0.3),
+          style: BorderStyle.solid,
+          width: 0.75
+        ),
+      ),
+      height: 100.0,
+      width: 100.0,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(top: 8.0,left: 15.0),
+child: Text(
+  cardTitle,
+  style: TextStyle(
+    fontSize: 12.0,
+    color: cardTitle== selectedCard ? Colors.white: Colors.grey.withOpacity(0.2),
+  ),
+),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 15.0,bottom: 8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  info, style: TextStyle(
+                  fontSize: 14.0,
+                  color: cardTitle== selectedCard?
+                  Colors.blueGrey: Colors.blue.shade800,
+                  fontWeight: FontWeight.bold
+                ),
+                ),
+                Text(unit,
+                style: TextStyle(
+                  fontSize: 12.0,
+                  color: cardTitle== selectedCard?
+                      Colors.blueGrey: Colors.blue.shade800,
+                ),)
+              ],
+            ),
+          )
+        ],
+      ),
+    ),
+  );
+}
+
+selectCard (cardTitle) {
+  setState(() {
+    selectedCard = cardTitle;
+  });
+}
+}
